c894c0d37f9cccef46abc5b520f06c1d8baf0ac0
Welberrr <welberhenrique.costa@gmail.com>
2025-12-03 18:31:00 -0300

    Feat: Criando a pagina principal do projeto

--- produtos/src/app/app.ts
+++ produtos/src/app/app.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface Produto{
+  nome: string;
+  valor: number;
+  imagem: string;
+}
+
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('produtos');
+  produtos: Produto[] = [
+    {
+      nome: 'Produto 1',
+      valor: 100,
+      imagem: 'https://via.placeholder.com/150'
+    },
+    {
+      nome: 'Produto 2',
+      valor: 200,
+      imagem: 'https://via.placeholder.com/150'
+    },
+    {
+      nome: 'Produto 3',
+      valor: 300,
+      imagem: 'https://via.placeholder.com/150'
+    }
+  ];
 }
